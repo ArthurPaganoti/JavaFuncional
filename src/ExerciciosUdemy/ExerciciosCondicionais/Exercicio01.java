a1c7a794359aa90e15f4c03dad4a492e1e1f8e04
Arthur Belarmino Paganoti <paganotiarthur@gmail.com>
2023-11-02 23:24:42 -0300

    Atualização

--- src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
+++ src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
@@ -2,6 +2,6 @@ package ExerciciosUdemy.ExerciciosCondicionais;
 
 public class Exercicio01 {
     public static void main(String[] args) {
-        System.out.println("Oi");
+        System.out.println("Oii");
     }
 }
