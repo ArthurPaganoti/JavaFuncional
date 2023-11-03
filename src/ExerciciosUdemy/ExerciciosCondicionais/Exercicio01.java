ad774b9387ad62b16c6f58be9a9b9ddd2618f641
Arthur Belarmino Paganoti <paganotiarthur@gmail.com>
2023-11-02 23:15:09 -0300

    Atualização no Exercicio01

--- src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
+++ src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
@@ -2,6 +2,19 @@ package ExerciciosUdemy.ExerciciosCondicionais;
 
 public class Exercicio01 {
     public static void main(String[] args) {
-        System.out.println("Meu primeiro comando em Java!");
+        int x = 5;
+        int y = 10;
+        if (x < 10) {
+            System.out.println("Olá");
+        }
+        if (y > 5) {
+            System.out.println("Bom dia");
+        }
+        if (x == 5) {
+            System.out.println("Boa tarde");
+        }
+        if (y < 20) {
+            System.out.println("Boa noite");
+        }
     }
 }
