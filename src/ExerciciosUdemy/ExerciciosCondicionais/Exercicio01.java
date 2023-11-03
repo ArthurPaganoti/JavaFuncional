db7ebeee0be0f1a2b847fcfd4635cac15fb325e0
Arthur Belarmino Paganoti <paganotiarthur@gmail.com>
2023-11-02 23:54:38 -0300

    Atualização no Exercicio01 na parte condicional dos exercícios da Udemy

--- src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
+++ src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
@@ -1,7 +1,31 @@
+// Enunciado:
+// Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+
 package ExerciciosUdemy.ExerciciosCondicionais;
+
+// Passo a Passo:
+// 1. Declarar a variável do tipo inteiro
+// 2. Ler o número inteiro
+// 3. Verificar se o número é negativo
+// 4. Imprimir o resultado
+
+import java.util.Scanner;
 
 public class Exercicio01 {
     public static void main(String[] args) {
-        // A fazer...
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+
+        System.out.println("Digite um número inteiro: ");
+        numero = sc.nextInt();
+
+        if (numero < 0) {
+            System.out.println("O número: " + numero + " é negativo");
+        } else {
+            System.out.println("O número : " +numero + " é postivo");
+        }
+        sc.close();
     }
 }
