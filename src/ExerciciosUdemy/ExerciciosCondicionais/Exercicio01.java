8b23d5f89cfc7fa4721dd14b0e49793b6f60d779
Arthur Belarmino Paganoti <paganotiarthur@gmail.com>
2023-11-02 23:43:42 -0300

    Atualização no Exercicio01 da parte condicional na udemy

--- src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
+++ src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
@@ -2,19 +2,6 @@ package ExerciciosUdemy.ExerciciosCondicionais;
 
 public class Exercicio01 {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 10;
-        if (x < 10) {
-            System.out.println("Olá");
-        }
-        if (y > 5) {
-            System.out.println("Bom dia");
-        }
-        if (x == 5) {
-            System.out.println("Boa tarde");
-        }
-        if (y < 20) {
-            System.out.println("Boa noite");
-        }
+        // A fazer...
     }
 }
