dc6eb23333c8750daad9bea0c95751dffd729f03
Arthur Belarmino Paganoti <paganotiarthur@gmail.com>
2023-11-03 14:39:58 -0300

    Atualização da parte condicional do Exercicio01 da lista de exercícios da Udemy

--- src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
+++ src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio01.java
@@ -14,18 +14,21 @@ import java.util.Scanner;
 public class Exercicio01 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // Criando o objeto Scanner
 
+        // Variaveis
         int numero;
 
+        // Pedindo para o usuario digitar o número e armazenando na váriavel
         System.out.println("Digite um número inteiro: ");
         numero = sc.nextInt();
 
-        if (numero < 0) {
-            System.out.println("O número: " + numero + " é negativo");
-        } else {
-            System.out.println("O número : " +numero + " é postivo");
+        // Condicionais
+        if (numero < 0) { // Sse o número for menor (<) que 0
+            System.out.println("O número: " + numero + " é negativo"); // Manda a mensagem dizendo que o número é negativo
+        } else { // Se não
+            System.out.println("O número : " +numero + " é postivo"); // Manda mensagem dizendo que o número é positivo
         }
-        sc.close();
+        sc.close(); // Fechando o objeto sc.
     }
 }
