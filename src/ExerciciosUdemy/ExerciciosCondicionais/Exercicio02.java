4bed799b57ad606f4355304b0080369dc2ac7245
Arthur Belarmino Paganoti <paganotiarthur@gmail.com>
2023-11-04 17:36:15 -0300

    Passei o enunciado do exercicio 2 da parte condicional, fiz o seu passo a passo, fiz a sua resolução e expliquei dentro do código o seu funcionamento e a lógica

--- src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio02.java
+++ src/ExerciciosUdemy/ExerciciosCondicionais/Exercicio02.java
@@ -1,7 +1,33 @@
+// Enunciado:
+// Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
 package ExerciciosUdemy.ExerciciosCondicionais;
+
+// Passo a Passo:
+// 1. Declarar a variável do tipo inteiro
+// 2. Ler o número inteiro
+// 3. Verificar se o número é par ou ímpar
+// 4. Imprimir o resultado
+
+import java.util.Scanner;
 
 public class Exercicio02 {
     public static void main(String[] args) {
-        System.out.println("Olá");
+
+        Scanner sc = new Scanner(System.in); // Criando o objeto Scanner para ler os valores
+
+        // Variaveis
+        int numero;
+
+        // Pedindo para o úsuario digitar o valor é armazanenando
+        System.out.println("Digite um número inteiro: ");
+        numero = sc.nextInt();
+
+        if (numero % 2 == 1) { // Se o número que o úsuario digitou tiver o resto da sua divisão igual a 1
+            System.out.println("O número: " + numero + " é impar");
+        } else { // Se não
+            System.out.println("O número: " + numero + " é par");
+        }
+        sc.close(); // Fechando o objeto Scanner
     }
 }
