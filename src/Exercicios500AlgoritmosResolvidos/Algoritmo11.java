3134476513f49d13a67475c229ac92987e2dbf5b
Arthur Belarmino Paganoti <paganotiarthur@gmail.com>
2023-11-27 14:59:22 -0300

    Criada a pasta para resolução de alguns algoritmos do livro 500 algoritmos resolvidos e feito e adicionado o algoritmo 11

---
+++ src/Exercicios500AlgoritmosResolvidos/Algoritmo11.java
@@ -0,0 +1,14 @@
+package Exercicios500AlgoritmosResolvidos;
+
+//prog impl
+//imprima "Aprendendo Algoritmo!!!";
+//f;mprog
+
+// Esse algoritmo faz com que seja exibida, na tela do computador, a
+// mensagem: Aprendendo Algoritmo!!!
+public class Algoritmo11 {
+    public static void main(String[] args) {
+
+        System.out.println("Aprendendo Algoritmo!!!");
+    }
+}
